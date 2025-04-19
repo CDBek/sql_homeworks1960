99126e172d3f8e2aa7143b66b956cae40eee05e3
CDBek <cdbeks@gmail.com>
2025-04-19 15:20:58 +0500

    Update begin2.sql

--- lesson-2/homework/begin2.sql
+++ lesson-2/homework/begin2.sql
@@ -1,4 +1,8 @@
-create table employees (EmpID INT, Name varchar (50), salary decimal (10,2));
+create table employees (
+EmpID INT, 
+Name varchar (50),
+salary decimal (10,2)
+);
 insert into employees (EmpID, Name, salary)
 values (1,'Olim_bayev', 50000000),
 (2,'Muzap', 60000000),
@@ -11,3 +15,23 @@ delete from employees
 where EmpID=2
 truncate table employees
 drop table	employees
+alter table employees
+alter column name varchar (100);
+alter table employees
+add Department varchar (50);
+alter table employees
+alter column salary float;
+
+select * from employees
+Delete from employees
+
+
+create table Departments (
+Departmentid int primary key,
+Departmentname varchar (50)
+);
+
+select * from Departments
+
+
+
